8c432560ef8f4d3526437c05d5ef7a7015b6a08b
wb_wangyu05 <wb_wangyu05@kuaishou.com>
2021-08-23 10:54:47 +0800

    有效序号问题

--- src/Algorithms/有效括号问题.js
+++ src/Algorithms/有效括号问题.js
@@ -22,55 +22,26 @@ const symbolMap = {
     '[': ']',
 }
 
-// function isValid(str) {
-//     if (!str) {
-//         return true;
-//     }
-//     const stack = new Stack();
-//     const len = str.length;
-//     for (let i = 0;i < len;i++) {
-//         const s = str[i];
-//         if (symbolMap[s]) {
-//             stack.push(symbolMap[s]);
-//         } else {
-//             const p = stack.pop();
-//             if (stack.isEmpty() || stack.pop() !== s) {
-//                 return false;
-//             }
-//         }
-//     }
-//     return !stack.size();
-// }
-
-const isValid = function (s) {
-    // 结合题意，空字符串无条件判断为 true
-    if (!s) {
+function isValid(str) {
+    if (!str) {
         return true;
     }
-    // 初始化 stack 数组
-    const stack = [];
-    // 缓存字符串长度
-    const len = s.length;
-    // 遍历字符串
-    for (let i = 0; i < len; i++) {
-        // 缓存单个字符
-        const ch = s[i];
-        // 判断是否是左括号，这里我为了实现加速，没有用数组的 includes 方法，直接手写判断逻辑
-        const symbolStr = symbolMap[ch];
+    const stack = new Stack();
+    const len = str.length;
+    for (let i = 0;i < len;i++) {
+        const s = str[i];
+        const symbolStr = symbolMap[s];
         if (symbolStr) {
             stack.push(symbolStr);
-        } else { // 若不是左括号，则必须是和栈顶的左括号相配对的右括号
-            // 若栈不为空，且栈顶的左括号没有和当前字符匹配上，那么判为无效
-            if (!stack.length || stack.pop() !== ch) {
+        } else {
+            if (!stack.size() || stack.pop() !== s) {
                 return false;
             }
         }
     }
-    // 若所有的括号都能配对成功，那么最后栈应该是空的
-    return !stack.length;
-};
+    return !stack.size();
+}
 
-
-console.log(isValid(str1));
-console.log(isValid(str2));
-console.log(isValid(str3));
+console.log(isValid(str1)); // true
+console.log(isValid(str2)); // true
+console.log(isValid(str3)); // false
